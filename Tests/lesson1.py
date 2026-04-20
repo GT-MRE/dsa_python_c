3fb81359afff87bcb80bf771f5272a119838ae40
GT-MRE <edenmike.ruhanga@strathmore.edu>
2026-04-20 12:09:47 +0300

    Basics

---
+++ Tests/lesson1.py
@@ -0,0 +1,18 @@
+name = "John"
+num = 25
+balance = 100.50
+flag = True
+
+# print(id(name))
+
+# print(type(name)) # <class 'str'>
+# print(type(num)) # <class 'int'>
+# print(type(balance)) # <class 'float'>
+# print(type(flag)) # <class 'bool'>
+
+# for i in range(0,10,2):
+#     print(i)
+
+items = ["DSA","COA","OOP"]
+for item in items:
+    print(item)
